fdbf5fe49de1b9e2d1f01b86cfa5632b595a9aa6
Rui <liurui320916@gmail.com>
2018-05-05 16:24:50 -0500

    Created Property Model and PropertyAdapter for RecyclerView of Property

---
+++ app/src/main/java/com/example/sravanreddy/realestateproject/adapters/PropertyAdapter.kt
@@ -0,0 +1,71 @@
+package com.example.sravanreddy.realestateproject.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.sravanreddy.realestateproject.R
+import com.example.sravanreddy.realestateproject.models.Property
+
+
+class PropertyAdapter(var properties: List<Property>,
+                  var clickListener: View.OnClickListener, var mContext: Context) :
+        RecyclerView.Adapter<PropertyAdapter.PropertyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PropertyViewHolder {
+        return PropertyViewHolder(LayoutInflater.from(parent?.context).
+                inflate(R.layout.property_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: PropertyViewHolder, position: Int) {
+        val property = properties[position]
+        holder.watches.text=property.watches.toString()
+        holder.favorites.text = property.favorites.toString()
+        if(property.imgUrl1.isNotEmpty()){
+            Glide.with(mContext)
+                    .load(property.imgUrl1)
+                    .into(holder.propertyImg)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return properties.size
+    }
+
+
+    fun getPhoto(adapterPosition: Int) : Property{
+        return properties[adapterPosition]
+    }
+
+    inner class PropertyViewHolder(itemView: View) : RecyclerView.ViewHolder
+    (itemView) {
+        var type: TextView
+        var cost: TextView
+        var details: TextView
+        var address: TextView
+        var watches: TextView
+        var favorites: TextView
+        var propertyImg: ImageView
+        var favBtn: ImageButton
+        var watchBtn: ImageButton
+        init {
+            if (clickListener != null) {
+                itemView.setOnClickListener(clickListener)
+            }
+            itemView.tag = this
+            type = itemView.findViewById(R.id.property_type) as TextView
+            cost = itemView.findViewById(R.id.property_price) as TextView
+            details = itemView.findViewById(R.id.property_detail) as TextView
+            address = itemView.findViewById(R.id.property_address) as TextView
+            watches = itemView.findViewById(R.id.watches) as TextView
+            favorites = itemView.findViewById(R.id.favorites) as TextView
+            favBtn = itemView.findViewById(R.id.favorite_white_label) as ImageButton
+            watchBtn = itemView.findViewById(R.id.watch_white_label) as ImageButton
+            propertyImg = itemView.findViewById(R.id.property_image) as ImageView
+        }
+    }
+}
