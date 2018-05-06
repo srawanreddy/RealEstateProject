f49e9e3a2e50128e29288ee1314ee10fc2c18c0b
Rui <liurui320916@gmail.com>
2018-05-06 17:10:09 -0500

    changed seller activity UI to be consistent with buyer

--- app/src/main/java/com/example/sravanreddy/realestateproject/view/activity/SellerActivity.kt
+++ app/src/main/java/com/example/sravanreddy/realestateproject/view/activity/SellerActivity.kt
@@ -2,6 +2,7 @@ package com.example.sravanreddy.realestateproject.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sravanreddy.realestateproject.R
 import com.example.sravanreddy.realestateproject.view.fragment.PropertyListFragment
 
@@ -9,7 +10,8 @@ class SellerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_property)
-        supportFragmentManager.beginTransaction().replace(R.id.PropertyFragmentContainer, PropertyListFragment()).commit()
+        setContentView(R.layout.activity_seller)
+        Log.d("SellerActivity", "Adding PropertyList Fragment")
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentSellerContainer, PropertyListFragment()).commit()
     }
 }
--- app/src/main/java/com/example/sravanreddy/realestateproject/view/fragment/PropertyListFragment.kt
+++ app/src/main/java/com/example/sravanreddy/realestateproject/view/fragment/PropertyListFragment.kt
@@ -1,11 +1,10 @@
 package com.example.sravanreddy.realestateproject.view.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
@@ -19,13 +18,10 @@ class PropertyListFragment : Fragment(), OnClickListener{
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        //mContext = context
-    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d("PropertyListFragment", "Creating List Fragment")
         val mView = inflater.inflate(R.layout.fragment_property_list, container, false)
+        Log.d("PropertyListFragment", "Inflated");
         val recyclerView:RecyclerView? = mView?.findViewById(R.id.propertyList_recycler)
         var imgs:Array<String> = context!!.resources.getStringArray(R.array.dummy_pics)
         var propertyList:MutableList<Property> = mutableListOf()
@@ -33,10 +29,9 @@ class PropertyListFragment : Fragment(), OnClickListener{
             val property = Property(i.toString(), "123", imgs[i])
             propertyList.add(property)
         }
-        var propertyAdapter:PropertyAdapter = PropertyAdapter(propertyList, this, context!!)
+        var propertyAdapter = PropertyAdapter(propertyList, this, context!!)
         recyclerView!!.adapter =propertyAdapter
         recyclerView!!.layoutManager = LinearLayoutManager(context!!)
-
         return mView
     }
 
