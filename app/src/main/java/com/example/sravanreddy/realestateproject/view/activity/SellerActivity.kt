c12c344c1a8a438bf614178eb407bea2957f5e44
Rui <liurui320916@gmail.com>
2018-05-06 18:55:25 -0500

    Added Simple MapView for seller

--- app/src/main/java/com/example/sravanreddy/realestateproject/view/activity/SellerActivity.kt
+++ app/src/main/java/com/example/sravanreddy/realestateproject/view/activity/SellerActivity.kt
@@ -2,16 +2,20 @@ package com.example.sravanreddy.realestateproject.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.sravanreddy.realestateproject.R
+import com.example.sravanreddy.realestateproject.view.fragment.BoundaryFragment
 import com.example.sravanreddy.realestateproject.view.fragment.PropertyListFragment
+import kotlinx.android.synthetic.main.activity_seller.*
 
 class SellerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller)
-        Log.d("SellerActivity", "Adding PropertyList Fragment")
         supportFragmentManager.beginTransaction().replace(R.id.fragmentSellerContainer, PropertyListFragment()).commit()
+        tv_mapView_seller.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentSellerContainer, BoundaryFragment()).commit()
+        }
+
     }
 }
--- app/src/main/java/com/example/sravanreddy/realestateproject/view/fragment/PropertyListFragment.kt
+++ app/src/main/java/com/example/sravanreddy/realestateproject/view/fragment/PropertyListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
@@ -19,9 +18,7 @@ class PropertyListFragment : Fragment(), OnClickListener{
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d("PropertyListFragment", "Creating List Fragment")
         val mView = inflater.inflate(R.layout.fragment_property_list, container, false)
-        Log.d("PropertyListFragment", "Inflated");
         val recyclerView:RecyclerView? = mView?.findViewById(R.id.propertyList_recycler)
         var imgs:Array<String> = context!!.resources.getStringArray(R.array.dummy_pics)
         var propertyList:MutableList<Property> = mutableListOf()
